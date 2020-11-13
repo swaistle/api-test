aedfb4b290340a0515a37279173aa891630b65fe
Samantha Waistle <swaistle@gmail.com>
2020-11-13 23:03:11 +0000

    test: instructions schema

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # BPDTS Technical Test
 
+## How to run the tests
+To run all the tests:
+* Terminal: `mvn test`
+* IntelliJ: Right click on `bpdts.tests` and select `Run` from the drop down
+
+The application url has a default set, but can be overridden with an environment variable:
+* Terminal: `APP_URL=<hostURI> mvn test` 
+* IntelliJ: `Edit Configuration` > in `Environment variables` field enter `APP_URL=<hostURI>` > Apply. Then you can run the test as normal.
--- src/test/java/bpdts/tests/InstructionsTests.java
+++ src/test/java/bpdts/tests/InstructionsTests.java
@@ -1,6 +1,6 @@
 package bpdts.tests;
 
-import bpdts.utility.ScenarioContext;
+import bpdts.utility.Environment;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 
@@ -9,9 +9,9 @@ import java.io.IOException;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.equalTo;
 
-public class InstructionsTest {
+public class InstructionsTests {
 
-    public String uri = ScenarioContext.getUrl() + "/instructions";
+    public String uri = Environment.getAppUrl() + "/instructions";
 
     @Test
     public void assertStatusEquals200() throws IOException {
@@ -31,6 +31,16 @@ public class InstructionsTest {
                 .assertThat()
                 .body("todo",
                         equalTo("Create a short automated test for this API. Check that the data returned by the API is valid, and that ensure that each valid operation can be successfully called for each endpoint. Once you've built the tests, push the answer to Github or Gitlab, and send us a link. "));
+    }
+
+    @Test
+    public void assertSchema() {
+        RestAssured.given()
+                .when()
+                .get(uri)
+                .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("InstructionsSchema.json"));
     }
 
 }
