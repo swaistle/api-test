46cbad28574705a724c626867ba738efe1ed15d9
Samantha Waistle <swaistle@gmail.com>
2020-11-13 21:53:42 +0000

    test: instructions response body

--- src/test/java/bpdts/tests/InstructionsTest.java
+++ src/test/java/bpdts/tests/InstructionsTest.java
@@ -23,4 +23,14 @@ public class InstructionsTest {
                 .statusCode(200);
     }
 
+    @Test public void assertResponse() throws IOException {
+        RestAssured.given()
+                .when()
+                .get(uri)
+                .then()
+                .assertThat()
+                .body("todo",
+                        equalTo("Create a short automated test for this API. Check that the data returned by the API is valid, and that ensure that each valid operation can be successfully called for each endpoint. Once you've built the tests, push the answer to Github or Gitlab, and send us a link. "));
+    }
+
 }
