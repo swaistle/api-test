de3faf4702e0c6008ff3399cea13db82502d08fa
Samantha Waistle <swaistle@gmail.com>
2020-11-19 12:35:43 +0000

    chore: update to BDD

--- src/test/java/bpdts/stepdefinitions/CityStepDefinitions.java
+++ src/test/java/bpdts/stepdefinitions/CityStepDefinitions.java
@@ -4,7 +4,11 @@ import bpdts.pages.EncodeString;
 import bpdts.pages.Environment;
 import bpdts.pages.RandomCityGenerator;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,44 +21,45 @@ public class CityStepDefinitions {
 
     public String uri = Environment.getAppUrl() + "/city/%s/users";
 
-    @Given("the city api status code is 200")
-    public void assertStatusEquals200() {
+    public Response response;
+    private ValidatableResponse json;
+
+    @Given("I make a request for a city")
+    public void randomRequest(){
         String uri = Environment.getAppUrl() + "/city/" + EncodeString.encode(RandomCityGenerator.randomCity()) + "/users";
         LOG.debug("uri: " + uri);
 
-        RestAssured.given()
-                .when()
-                .get(uri)
-                .then()
-                .assertThat()
-                .statusCode(200);
+        RequestSpecification request = RestAssured.given();
+
+        response = request.get(uri);
     }
 
-    @Given("^user(?:s)? from (.*) matches the schema$")
-    public void assertSchema(String city) {
+    @Given("^I make a request for the city (.*)$")
+    public void specifiedRequest(String city){
         String formattedUri = String.format(uri, EncodeString.encode(city));
         LOG.debug("uri: " + formattedUri);
 
-        RestAssured.given()
+        response = RestAssured.given()
                 .when()
-                .get(formattedUri)
-                .then()
+                .get(formattedUri);
+    }
+
+    @Then("the city api status code is 200")
+    public void assertStatusEquals200() {
+        response.then().assertThat().statusCode(200);
+    }
+
+    @Then("the response matches the schema")
+    public void matchesSchema() {
+        response.then()
                 .assertThat()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("schemas/CitySchema.json"));
-
     }
 
-    @Given("^the city api content-type is application/json$")
+    @Given("^the response content-type is application/json$")
     public void assertContentType(){
-        String uri = Environment.getAppUrl() + "/city/" + EncodeString.encode(RandomCityGenerator.randomCity()) + "/users";
-        LOG.debug("uri: " + uri);
-
-        RestAssured.given()
-                .when()
-                .get(uri)
-                .then()
-                .assertThat()
+        response.then().assertThat()
                 .header("content-type", "application/json");
     }
 
